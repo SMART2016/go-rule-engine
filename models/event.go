a682261e3db640e990f786b5ec51acc2c9525343
Dipanjan Mazumder <dipanjan.mazumder@perforce.com>
2025-02-28 17:09:25 +0530

    Added Phase 1 Implementation of stateful Rule Processing library

--- models/event.go
+++ models/event.go
@@ -3,8 +3,9 @@ package models
 import "time"
 
 type Event[T any] struct {
-	TenantID     string    `json:"tenant_id"`
-	Type         string    `json:"type"`
+	TenantID string `json:"tenant_id"`
+	Type     string `json:"type"`
+	//TODO: Handle dynamic payload schema
 	Payload      T         `json:"payload"`
 	ShouldHandle bool      `json:"should_handle"`
 	EventSHA     string    `json:"event_sha"`
