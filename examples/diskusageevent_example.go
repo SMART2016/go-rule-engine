1c3c959cc1aa65ec5dd8a3c15fa8123323dadacc
Dipanjan Mazumder <dipanjan.mazumder@perforce.com>
2025-03-03 16:56:40 +0530

    Added Example code to show the usage of the framework

--- examples/diskusageevent_example.go
+++ examples/diskusageevent_example.go
@@ -1,4 +1,4 @@
-package events
+package examples
 
 import (
 	"context"
--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "github.com/SMART2016/go-rule-engine/examples"
+import (
+	"context"
+	"fmt"
+	"github.com/SMART2016/go-rule-engine/examples"
+	"github.com/SMART2016/go-rule-engine/models"
+	ruleprocessor "github.com/SMART2016/go-rule-engine/rule-processor"
+	"log"
+	"time"
+)
 
 func main() {
-	examples.ExampleRuleProcessor()
+
+	//Initialize the config instance
+	opts := []ruleprocessor.FrameworkConfigOption{
+		ruleprocessor.WithDBConfigPath("configs/db_config.json"),
+		ruleprocessor.WithRuleRepoPath("configs/rules.json"),
+		ruleprocessor.WithCleanupInterval(1 * time.Hour),
+	}
+	//Initialize basic Configs
+	config, err := ruleprocessor.NewFrameworkConfig(opts...)
+	if err != nil {
+		log.Fatalf("Error initializing framework config: %v", err)
+	}
+
+	// Initialize Rule Processor
+	processor, err := ruleprocessor.NewGRuleProcessor(config)
+	if err != nil {
+		log.Fatalf("Error initializing rule processor: %v", err)
+	}
+
+	// Initialize event registry
+	registry := models.GetEventRegistry()
+	// Step 1: Register event types (only once at startup)
+	registry.RegisterEventType("disk_space", func() models.Evaluable {
+		return &examples.DiskUsageEvent{}
+	})
+
+	// Example JSON input for a disk usage event
+	rawJSON := []byte(`{
+		"payload": {
+			"usage_percentage": 80,
+			"instance_id": "abcd",
+			"disk_size_in_bytes": 2048
+		},
+		"tenant_id": "tenant_1",
+		"type": "disk_space"
+	}`)
+
+	// Step 2: Process the event dynamically
+	ctx := context.Background()
+	handled, err := registry.ProcessEvent(ctx, processor, rawJSON)
+	if err != nil {
+		log.Fatalf("Error processing event: %v", err)
+	}
+	if handled {
+		fmt.Println("Event processed, action triggered.")
+	} else {
+		fmt.Println("Event processed, no action needed.")
+	}
 }
