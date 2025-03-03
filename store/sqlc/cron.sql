6a16d7de1fa3a5d23a7ec1a16393bcf3edfa0cfd
Dipanjan Mazumder <dipanjan.mazumder@perforce.com>
2025-03-03 12:38:20 +0530

    Added Custom postgres image with pgcron support

--- store/sqlc/cron.sql
+++ store/sqlc/cron.sql
@@ -2,10 +2,16 @@ SELECT cron.schedule(
                'event_cleanup',  -- Job name
                '*/10 * * * *',   -- Runs every 10 minutes
                $$WITH rows_to_delete AS (
-        SELECT ctid FROM processed_events
+        SELECT tenant_id FROM processed_events
         WHERE occurred_at < NOW() - INTERVAL '1 hour' * 24  -- TTL = 24 hours
         LIMIT 5000
     )
     DELETE FROM processed_events USING rows_to_delete
     WHERE processed_events.ctid = rows_to_delete.ctid;$$
-);
+       );
+
+SELECT cron.schedule(
+               'event_cleanup_vacuum',
+               '*/30 * * * *',  -- Runs every 30 minutes
+               $$VACUUM ANALYZE processed_events;$$
+       );
--- store/sqlc/store_schema.sql
+++ store/sqlc/store_schema.sql
@@ -22,23 +22,6 @@ ALTER TABLE processed_events SET (
     );
 
 
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-SELECT cron.schedule(
-               'event_cleanup',  -- Job name
-               '*/10 * * * *',   -- Runs every 10 minutes
-               $$WITH rows_to_delete AS (
-        SELECT tenant_id FROM processed_events
-        WHERE occurred_at < NOW() - INTERVAL '1 hour' * 24  -- TTL = 24 hours
-        LIMIT 5000
-    )
-    DELETE FROM processed_events USING rows_to_delete
-    WHERE processed_events.ctid = rows_to_delete.ctid;$$
-);
-
-SELECT cron.schedule(
-               'event_cleanup_vacuum',
-               '*/30 * * * *',  -- Runs every 30 minutes
-               $$VACUUM ANALYZE processed_events;$$
-       );
+--CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 
